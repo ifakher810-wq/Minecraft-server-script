8b7260c517d60d0ecc96b94fef2663553fb1ef50
ifakher810-wq <ifakher810@gmail.com>
2026-06-17 01:03:49 +0500

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BASE_DIR="$HOME/mc-servers"
+DEFAULT_RAM="1024"
+SERVER_VERSION="paper"
+
+PAPER_URL="https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/latest/downloads/paper.jar"
